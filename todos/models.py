6d4fd15804bcb687427e9be07c4a41beaf92d08b
sypai <7suyashbajpai@gmail.com>
2020-03-18 00:33:05 +0530

    Improved the Dashboard, fixed the issue of all todos loading ofor all users

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -8,7 +11,24 @@ class Todo(models.Model):
     todo_date = models.DateTimeField(default=datetime.now)
     title = models.CharField(max_length=50)
     user_id = models.IntegerField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
     is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_new = models.BooleanField(default=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Todo
@@ -11,6 +12,10 @@ def add_todo(request):
 
         _todo = Todo(todo=todo, title=title, user_id=user_id)
         _todo.save()
+
+        user = User.objects.get(pk=user_id)
+        user.profile.is_new = False
+        user.save()
 
         Todos = Todo.objects.all()
         context = {
